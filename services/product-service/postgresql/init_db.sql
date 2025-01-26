757fefbce067c943417f9f72e8023fb7c97a2ddf
Boklap <setanmarco@gmail.com>
2025-01-26 18:36:42 +0700

    sdf

--- services/product-service/postgresql/init_db.sql
+++ services/product-service/postgresql/init_db.sql
@@ -1,34 +1,14 @@
--- Check if the database 'bokman' exists
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'bokman') THEN
-        -- Create the database if it doesn't exist
-        CREATE DATABASE bokman;
-    END IF;
-END $$;
+CREATE DATABASE bokman;
 
--- Connect to the 'bokman' database
-\c bokman
+\c bokman;
 
--- Check if the 'products' table exists, and create it only if it doesn't
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'products') THEN
-        CREATE TABLE products (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            price NUMERIC(10, 2) NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );
-    END IF;
-END $$;
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price NUMERIC(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
--- Insert default data if the 'products' table is empty
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM products) THEN
-        INSERT INTO products (name, price) VALUES
-        ('Jacket', 20000),
-        ('Trouser', 30000);
-    END IF;
-END $$;
+INSERT INTO products ( name, price ) VALUES
+('Jacket', 20000),
+('Trouser', 30000)
