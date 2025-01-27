762e23cc7f1bb77cf17ac134d52fffaa98b405ea
Boklap <setanmarco@gmail.com>
2025-01-27 13:52:05 +0700

    Update run.sh and stop.sh

--- services/payment-service/redis/init_db.sh
+++ services/payment-service/redis/init_db.sh
@@ -4,5 +4,12 @@ redis-server /usr/local/etc/redis/redis.conf
 
 sleep 5
 
+echo "hehe"
+
+redis-cli
+
+
+
+
 redis-cli set payment:1 '{"order_id":1,"amount":100,"status":"completed"}'
 redis-cli set payment:2 '{"order_id":2,"amount":200,"status":"pending"}'
--- services/stop.sh
+++ services/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-services=("user-service" "product-service" "order-service" "payment-service")
+services=("user-service" "product-service" "order-service" "payment-service" "notification-service")
 
 for service in "${services[@]}"; do
   cd $service
