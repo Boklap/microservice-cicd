4e97ccbccf22e7210b3ae4b25c5a5e811993893a
Boklap <setanmarco@gmail.com>
2025-01-27 13:36:17 +0700

    Update redis init_db.sh

--- services/payment-service/redis/init_db.sh
+++ services/payment-service/redis/init_db.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-redis-server --daemonize yes
+redis-server /usr/local/etc/redis/redis.conf
+
+sleep 5
+
 redis-cli set payment:1 '{"order_id":1,"amount":100,"status":"completed"}'
 redis-cli set payment:2 '{"order_id":2,"amount":200,"status":"pending"}'
-redis-cli shutdown
-redis-server /usr/local/etc/redis/redis.conf
