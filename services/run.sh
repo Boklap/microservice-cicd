f68be6687c46061c2e5b83a739ac598cd93ad018
Boklap <setanmarco@gmail.com>
2025-01-27 13:26:32 +0700

    Update run.sh and stop.sh

--- services/run.sh
+++ services/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-services=("user-service" "product-service" "order-service", "payment-service")
+services=("user-service" "product-service" "order-service" "payment-service")
 
 for service in "${services[@]}"; do
   echo "Building and starting $service..."
